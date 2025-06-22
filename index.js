7572d1b55a70612452e8a5277ef714f263e14732
sahaarnav3 <sahaarnav3@gmail.com>
2025-06-22 19:07:14 +0530

    Added route to delete a Lead from DB.

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ app.post("/leads", async (req, res) => {
     if (!req.body.priority)
       return res
         .status(400)
-        .json({ error: "Invalid input: 'priority' is required." });
+        .json({ error: "Invalid input: 'priority' is required. Choose between ['Low', 'Medium', 'High']" });
 
     const saveLead = await lead.save();
     res.status(200).json(saveLead);
@@ -212,6 +212,22 @@ app.put("/leads/:id", async (req, res) => {
       return res.status(404).json({error: `Lead with ID '${req.params.id}' not found.`});
     else
       return res.status(200).json(leadResponse);
+  } catch (error) {
+    console.log(err);
+    res.status(500).json({
+      error:
+        "Some error occurred with the request itself. Please check logs and try again.",
+    });
+  }
+});
+
+//Route to delete a lead.
+app.delete("/leads/:id", async(req, res) => {
+  try {
+    const leadResponse = await Lead.findByIdAndDelete(req.params.id);
+    if(!leadResponse)
+      return res.status(404).json({error: `Lead with ID '${req.params.id}' not found.`});
+    return res.status(200).json({"message": "Lead deleted successfully."});
   } catch (error) {
     console.log(err);
     res.status(500).json({
